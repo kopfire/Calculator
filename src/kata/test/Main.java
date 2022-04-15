bd486586e1b8fec8764f0ce1d852ab90a45908e5
kopfire <kopfireorganisation@gmail.com>
2022-04-15 11:52:08 +0400

    some refactoring

--- src/kata/test/Main.java
+++ src/kata/test/Main.java
@@ -11,9 +11,9 @@ public class Main {
             if (input.equals("exit"))
                 break;
             String[] arrayInput = Arrays.stream(input.split("[+*\\-/]")).map(String::trim).toArray(String[]::new);
-            if (arrayInput.length != 2 || input.contains("."))
+            if (arrayInput.length != 2)
                 throw new Exception("Invalid input");
-            if (arrayInput[0].matches(" *\\d+ *") && arrayInput[1].matches(" *\\d+ *"))
+            if (arrayInput[0].matches("\\d+") && arrayInput[1].matches("\\d+"))
                 System.out.println(math(input, Integer.parseInt(arrayInput[0]), Integer.parseInt(arrayInput[1])));
             else {
                 int x = toArabian(arrayInput[0]);
@@ -31,8 +31,19 @@ public class Main {
 
     public static String toRoman(int arabian) {
         StringBuilder output = new StringBuilder();
-        Map<Integer, String> dictionary = new TreeMap<>(Collections.reverseOrder()); { dictionary.put(100,"C"); dictionary.put(90,"XC"); dictionary.put(50,"L"); dictionary.put(40,"XL"); dictionary.put(10,"X"); dictionary.put(9,"IX"); dictionary.put(5,"V"); dictionary.put(4,"IV"); dictionary.put(1,"I");}
-        for (int i : dictionary.keySet()){
+        Map<Integer, String> dictionary = new TreeMap<>(Collections.reverseOrder());
+        {
+            dictionary.put(100, "C");
+            dictionary.put(90, "XC");
+            dictionary.put(50, "L");
+            dictionary.put(40, "XL");
+            dictionary.put(10, "X");
+            dictionary.put(9, "IX");
+            dictionary.put(5, "V");
+            dictionary.put(4, "IV");
+            dictionary.put(1, "I");
+        }
+        for (int i : dictionary.keySet()) {
             output.append(dictionary.get(i).repeat(arabian / i));
             arabian -= arabian / i * i;
         }
